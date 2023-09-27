6e4cc65aa6028765a477dd5bb62324f086493d8d
xj99051 <1634112975@qq.com>
2023-09-27 23:13:38 +0800

    Initial commit

---
+++ src/main/java/com/ecust/appv1boatbackend/AppV1BoatBackendApplication.java
@@ -0,0 +1,13 @@
+package com.ecust.appv1boatbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppV1BoatBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppV1BoatBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ecust/appv1boatbackend/AppV1BoatBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ecust.appv1boatbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppV1BoatBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
